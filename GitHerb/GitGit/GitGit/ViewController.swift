9b0a257d629a455d243c636ada8bb60cdb340f25
ppp <ppp>
2022-04-11 16:05:52 +0900

    GitHerb

---
+++ GitHerb/GitGit/GitGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitGit
+//
+//  Created by max on 2022/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
